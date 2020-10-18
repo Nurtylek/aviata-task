4491f4944e15d2c710a794e3521cf687fc4acd7b
Нуртилек Кошкар <nurtylek98@gmail.com>
2020-10-18 22:24:04 +0600

    fix linting

--- src/components/TicketList.vue
+++ src/components/TicketList.vue
@@ -1,19 +1,20 @@
 <template>
-    <app-ticket-list-item
-        :ticket="ticket"
-        v-for="ticket in filteredTickets" :key="ticket.id"
-        class="marginBottom-12"
-    ></app-ticket-list-item>
+  <app-ticket-list-item
+    :ticket="ticket"
+    v-for="ticket in filteredTickets"
+    :key="ticket.id"
+    class="marginBottom-12"
+  ></app-ticket-list-item>
 </template>
 
 <script>
 import TicketListItem from "./TicketListItem.vue";
 
 export default {
-    name: "TicketList",
-    props: ["filteredTickets"],
-    components: {
-        appTicketListItem: TicketListItem
-    }
+  name: "TicketList",
+  props: ["filteredTickets"],
+  components: {
+    appTicketListItem: TicketListItem
+  }
 };
 </script>
